8e449430513e4ece2950c23064afa895afdb20f6
Jose Terrera <joseterrera@yahoo.com>
2020-10-07 11:05:01 -0700

    working on third part

--- deck-of-cards/app.js
+++ deck-of-cards/app.js
@@ -31,3 +31,8 @@ async function getTwoCards() {
 }
 
 getTwoCards()
+
+
+/**
+ * Build an HTML page that lets you draw cards from a deck. When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck.
+ */
