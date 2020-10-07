5f5d5cb571c5cb481be9299dd77ef4a30f754160
Jose Terrera <joseterrera@yahoo.com>
2020-10-07 12:41:00 -0700

    rendering cards with translate

--- deck-of-cards/app.js
+++ deck-of-cards/app.js
@@ -36,3 +36,38 @@ getTwoCards()
 /**
  * Build an HTML page that lets you draw cards from a deck. When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck.
  */
+
+const button = document.querySelector('button')
+const cardContainer = document.querySelector('div.card-container')
+const listEl = document.querySelector('ul')
+
+function generateHTML(srcLink){
+  let angle = Math.random() * 90 - 45;
+  let randomX = Math.random() * 40 - 20;
+  let randomY = Math.random() * 40 - 20;
+  return `<li><img src=${srcLink} class="img" style="transform: translate(${randomX}px, ${randomY}px) rotate(${angle}deg)"/></li>`
+}
+
+
+
+ async function drawCards() {
+  let cardsDrawed = []
+  let response = await axios.get(`${baseURL}/new/shuffle`)
+  let deckId = response.data.deck_id;
+
+  button.addEventListener('click', async function() {
+    let newCard = await axios.get(`${baseURL}/${deckId}/draw`)
+
+    let cardSrc = newCard.data.cards[0].image;
+    let cardsRemaining = newCard.data.remaining;
+
+    cardsDrawed.push(generateHTML(cardSrc))
+    listEl.innerHTML = cardsDrawed.join('\n\n')
+    if (cardsRemaining === 0) {
+      button.style.cursor = 'not-allowed'
+      button.style.pointerEvents = 'none'
+    }
+  })
+ }
+
+ drawCards()
