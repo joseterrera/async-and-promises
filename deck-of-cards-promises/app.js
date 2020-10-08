d780317fff6ea80f2f0b1ab3448629a1b08cf4b6
Jose Terrera <joseterrera@yahoo.com>
2020-10-07 21:48:17 -0700

    finished third case of promises

--- deck-of-cards-promises/app.js
+++ deck-of-cards-promises/app.js
@@ -67,6 +67,9 @@ function generateHTML(srcLink){
 
 
 //Exercise 3
+/**
+ * Build an HTML page that lets you draw cards from a deck. When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck.
+ */
 
 let deckIdCard = null
 let cardsRemaining = null
@@ -87,14 +90,16 @@ let cardsDrawed = []
       res => {
         let cardsRemaining = res.data.remaining;
         let cardSrc = res.data.cards[0].image;  
-        cardsDrawed.push(generateHTML(cardSrc))
-        listEl.innerHTML = cardsDrawed.join('\n\n')
-        if (cardsRemaining === 0) {
+        if (cardsRemaining === 0 || cardSrc === undefined) {
           button.style.cursor = 'not-allowed'
           button.style.pointerEvents = 'none'
           button.style.backgroundColor = '#ddd'
-          cardSrc = null
+
         }
+
+        cardsDrawed.push(generateHTML(cardSrc))
+        listEl.innerHTML = cardsDrawed.join('\n\n')
+    
       }
     )
     
